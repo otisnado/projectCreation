7c76e55c8e8d365a1f47f3f06f86de8c91b31dc9
oncallejas <oncallejas@stefanini.com>
2022-08-24 16:31:13 -0600

    agregando variable de entorno de proyecto en lugar de ID del grupo

--- createQuery.js
+++ createQuery.js
@@ -6,12 +6,12 @@ var queries = [
     {
         "name": "projectBacklog_ReleasesAndUserStories",
         "path": "Shared Queries/projectBacklog_ReleasesAndUserStories",
-        "wiql": "SELECT [System.Id], [System.WorkItemType], [System.Title], [System.AssignedTo], [System.State], [System.Tags] FROM workitems WHERE [System.TeamProject] = @project AND [System.WorkItemType] IN ('Releases', 'Historia de usuario') AND [System.State] <> '' AND [System.AreaPath] = '605'"
+        "wiql": "SELECT [System.Id], [System.WorkItemType], [System.Title], [System.AssignedTo], [System.State], [System.Tags] FROM workitems WHERE [System.TeamProject] = @project AND [System.WorkItemType] IN ('Releases', 'Historia de usuario') AND [System.State] <> '' AND [System.AreaPath] = '"+ process.env.ADO_PROJECT +"'"
     },
     {
         "name": "impedimentsInCurrentSprint",
         "path": "Shared Queries/impedimentsInCurrentSprint",
-        "wiql": "SELECT [System.Id], [System.WorkItemType], [System.Title], [System.AssignedTo], [System.State], [System.Tags] FROM workitems WHERE [System.TeamProject] = @project AND [System.WorkItemType] = 'Impediment' AND [System.IterationPath] = @currentIteration('[605]\\605 Team <id:3b752b25-952d-47ea-8c7b-c735f5027f61>')"
+        "wiql": "SELECT [System.Id], [System.WorkItemType], [System.Title], [System.AssignedTo], [System.State], [System.Tags] FROM workitems WHERE [System.TeamProject] = @project AND [System.WorkItemType] = 'Impediment' AND [System.IterationPath] = @currentIteration('["+ process.env.ADO_PROJECT +"]\\"+ process.env.ADO_PROJECT +" Team')"
     },
     {
         "name": "getAllUserStories",
@@ -21,17 +21,17 @@ var queries = [
     {
         "name": "backlogInCurrentSprintByState",
         "path": "Shared Queries/backlogInCurrentSprintByState",
-        "wiql": "SELECT [System.Id], [System.WorkItemType], [System.Title], [System.AssignedTo], [System.State], [System.Tags] FROM workitems WHERE [System.TeamProject] = @project AND [System.WorkItemType] = 'Historia de usuario' AND [System.State] IN ('En proceso', 'Listo', 'Por Hacer') AND [System.IterationPath] = @currentIteration('[605]\\605 Team <id:3b752b25-952d-47ea-8c7b-c735f5027f61>')"
+        "wiql": "SELECT [System.Id], [System.WorkItemType], [System.Title], [System.AssignedTo], [System.State], [System.Tags] FROM workitems WHERE [System.TeamProject] = @project AND [System.WorkItemType] = 'Historia de usuario' AND [System.State] IN ('En proceso', 'Listo', 'Por Hacer') AND [System.IterationPath] = @currentIteration('["+ process.env.ADO_PROJECT +"]\\"+ process.env.ADO_PROJECT +" Team')"
     },
     {
         "name": "tasksInCurrentSprint",
         "path": "Shared Queries/tasksInCurrentSprint",
-        "wiql": "SELECT [System.Id], [System.WorkItemType], [System.Title], [System.AssignedTo], [System.State], [System.Tags] FROM workitems WHERE [System.TeamProject] = @project AND [System.WorkItemType] = 'Task' AND [System.State] <> '' AND [System.IterationPath] = @currentIteration('[605]\\605 Team <id:3b752b25-952d-47ea-8c7b-c735f5027f61>')"
+        "wiql": "SELECT [System.Id], [System.WorkItemType], [System.Title], [System.AssignedTo], [System.State], [System.Tags] FROM workitems WHERE [System.TeamProject] = @project AND [System.WorkItemType] = 'Task' AND [System.State] <> '' AND [System.IterationPath] = @currentIteration('["+ process.env.ADO_PROJECT +"]\\"+ process.env.ADO_PROJECT +" Team')"
     },
     {
         "name": "testCasesAndBugsProject",
         "path": "Shared Queries/testCasesAndBugsProject",
-        "wiql": "SELECT [System.Id], [System.WorkItemType], [System.Title], [System.AssignedTo], [System.State], [System.Tags] FROM workitems WHERE [System.TeamProject] = @project AND [System.WorkItemType] IN ('Test Case', 'Bug') AND [System.State] <> '' AND [System.AreaPath] = '605'"
+        "wiql": "SELECT [System.Id], [System.WorkItemType], [System.Title], [System.AssignedTo], [System.State], [System.Tags] FROM workitems WHERE [System.TeamProject] = @project AND [System.WorkItemType] IN ('Test Case', 'Bug') AND [System.State] <> '' AND [System.AreaPath] = '"+ process.env.ADO_PROJECT +"'"
     }
 ]
 
