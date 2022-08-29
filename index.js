aae12feae39cd06d5d8d9a342fc03b82f843251e
oncallejas <oncallejas@stefanini.com>
2022-08-29 15:40:43 -0600

    agregando timeout para ejecutar createQueries

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 import { createProject } from "./createProject.js"
 import { createQueries } from "./createQuery.js"
-import { basicAuth, project_config, queries } from "./util/globalVariables.js"
-import { create_project_url, create_query_url } from "./util/uris.js"
+import { createWidgets } from "./createWidgets.js"
+import { basicAuth, project_config, queries, widgets } from "./util/globalVariables.js"
+import { create_project_url, create_query_url, create_widgets_url } from "./util/uris.js"
 
 createProject(create_project_url, project_config, basicAuth)
-createQueries(create_query_url, queries, basicAuth)
+setTimeout(function () {
+    createQueries(create_query_url, queries, basicAuth);
+}, 5000);
+//createQueries(create_query_url, queries, basicAuth)
+//setTimeout("Ya estuvo", 5000);
+//createWidgets(create_widgets_url, widgets, basicAuth)
