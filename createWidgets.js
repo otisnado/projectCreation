5cf415eb79996d3dc498e9a03b8085c43d9e4c70
oncallejas <oncallejas@stefanini.com>
2022-08-29 18:26:07 -0600

    limpiando proyecto

--- createWidgets.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios"
-import { getPrincipalDashboard } from "./util/dashboard.js"
-
-function createWidgets(url_in, widgets_in, basicAuth_in) {
-    const dashboard_info = getPrincipalDashboard()
-    console.log(dashboard_info)
-}
-
-export { createWidgets }
